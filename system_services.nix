229909eca134fa462529a59d842b7c713432873d
DestinyofYeet <olebend@gmail.com>
2024-07-15 17:23:45 +0200

    fixed indents

--- system_services.nix
+++ system_services.nix
@@ -1,68 +1,52 @@
 { config, pkgs, lib, modulesPath, ... }:
 
 {
-#systemd.services.innernet-mynet = {
-#    description = "innernet server for infra";
-#    wantedBy = [ "multi-user.target" ];
-#    after = [ "network-online.target" "nss-lookup.target" ];
-#    wants = [ "network-online.target" "nss-lookup.target" ];
-#    path = with pkgs; [ iproute ];
-#    environment = { RUST_LOG = "info"; };
-#    serviceConfig = {
-#      Restart = "always";
-#      ExecStart = "${innernet}/bin/innernet up infra --daemon --interval 60";
-#    };
-#  };
+    # enable openssh server
+    services.openssh.enable = true;
+    
+    networking.firewall = {
+    	# conduit: 6167
+    	# ssh: 22 (just to be sure)
+    	# innernet: 51820
+    	allowedTCPPorts = [ 6167 22 51820 ];
+    	allowedUDPPorts = [ 6167 22 51820 ];
+    };
 
 
-# enable openssh server
-services.openssh.enable = true;
-
-networking.firewall = {
-	# conduit: 6167
-	# ssh: 22 (just to be sure)
-	# innernet: 51820
-	allowedTCPPorts = [ 6167 22 51820 ];
-	allowedUDPPorts = [ 6167 22 51820 ];
-};
-
-
-# matrix conduit server, default port 6167
-services.matrix-conduit = {
-		enable = true;
-
-		settings.global = {
-			address = "0.0.0.0";
-			server_name = "matrix.ole.blue";
-			allow_registration = true;
-			registration_token = "VX3deHVAEU49CkAWLj3cPfk3BrMgBsGNnWAPV3kV";
-			enable_lightning_bolt = false;
-		};
-		
+    # matrix conduit server, default port 6167
+    services.matrix-conduit = {
+    		enable = true;
+    		settings.global = {
+    			address = "0.0.0.0";
+    			server_name = "matrix.ole.blue";
+    			allow_registration = true;
+    			registration_token = "VX3deHVAEU49CkAWLj3cPfk3BrMgBsGNnWAPV3kV";
+    			enable_lightning_bolt = false;
+    		};	
 	};
 
 
-services.nginx = {
-		enable = false;
-		recommendedProxySettings = true;
-		recommendedTlsSettings = true;
-		
-		virtualHosts."matrix.ole.blue" = {
-			locations."/" = {
-				proxyPass = "http://127.0.0.1:123";
-				proxyWebsockets = true;
-			};
-		};
+    services.nginx = {
+    		enable = false;
+    		recommendedProxySettings = true;
+    		recommendedTlsSettings = true;
+    		
+    		virtualHosts."matrix.ole.blue" = {
+    			locations."/" = {
+    				proxyPass = "http://127.0.0.1:123";
+    				proxyWebsockets = true;
+    			};
+    		};
 	};
 
-systemd.services.innernet-infra = {
-		description = "innernet client for infra";
-		wantedBy = [ "multi-user.target" ];
-		after = [ "network-online.target" "nss-lookup.target" ];
-		wants = [ "network-online.target" "nss-lookup.target" ];
-		serviceConfig = {
-			Restart = "always";
-			ExecStart = "${pkgs.innernet}/bin/innernet up infra --daemon --interval 60";
+    systemd.services.innernet-infra = {
+    		description = "innernet client for infra";
+    		wantedBy = [ "multi-user.target" ];
+    		after = [ "network-online.target" "nss-lookup.target" ];
+    		wants = [ "network-online.target" "nss-lookup.target" ];
+    		serviceConfig = {
+    			Restart = "always";
+    			ExecStart = "${pkgs.innernet}/bin/innernet up infra --daemon --interval 60";
 		};
 	};
 
--- system_users.nix
+++ system_users.nix
@@ -1,16 +1,15 @@
 { config, pkgs, lib, modulesPath, ... }:
 
 {
-
-users.users.ole = {
-	isNormalUser = true;
-	home = "/home/ole";
-	description = "me";
-	extraGroups = [ "wheel" ];
-	hashedPassword = "$6$s5ZWf9efO2lEySC0$ztuOgJsHnckwmcP5EEpgcDJeUpJD3ZJuynRIuuC.IEBLMBtkZS5R1JQ7c4a/oUU6Tp8eDWNUoHjckyL/hivvg1";
-	openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+VUI7U1fv0W6Lp40Jss9yA6JX+JG/Hocroff6HtlFT ole@kartoffelkiste" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhLrbWc/gopTJ2ZZW4ZfCzGhjhV9fKb1wdvFLQpmP3y ole@main" ];
-		packages = with pkgs; [
-			neovim
-		];
-	};
+    users.users.ole = {
+    	isNormalUser = true;
+    	home = "/home/ole";
+    	description = "me";
+    	extraGroups = [ "wheel" ];
+    	hashedPassword = "$6$s5ZWf9efO2lEySC0$ztuOgJsHnckwmcP5EEpgcDJeUpJD3ZJuynRIuuC.IEBLMBtkZS5R1JQ7c4a/oUU6Tp8eDWNUoHjckyL/hivvg1";
+    	openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+VUI7U1fv0W6Lp40Jss9yA6JX+JG/Hocroff6HtlFT ole@kartoffelkiste" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhLrbWc/gopTJ2ZZW4ZfCzGhjhV9fKb1wdvFLQpmP3y ole@main" ];
+    		packages = with pkgs; [
+    			neovim
+    		];
+    	};
 }
