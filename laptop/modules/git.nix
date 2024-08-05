0331f47b07f5ddb5aa07ffb776773c9b02b0a3ed
DestinyofYeet <olebend@gmail.com>
2024-08-05 21:48:33 +0200

    added git options

---
+++ laptop/modules/git.nix
@@ -0,0 +1,15 @@
+{ ... }:{
+  programs.git = {
+    enable = true;
+    userName = "DestinyofYeet";
+    userEmail = "olebend@gmail.com";
+  };
+
+  programs.lazygit = {
+    enable = true;
+  };
+
+  programs.gitui = {
+    enable = true;
+  };
+}
