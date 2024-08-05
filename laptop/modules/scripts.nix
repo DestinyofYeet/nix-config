6bc180df3c26d66c58841f3b789b58c76714a883
DestinyofYeet <olebend@gmail.com>
2024-08-05 22:50:26 +0200

    added scripts

---
+++ laptop/modules/scripts.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }: 
+let
+  scripts-dir = "${config.home.homeDirectory}/scripts";
+in {
+  home.file = {
+    "${scripts-dir}/update-needed-content.sh" = {
+      text = ''
+        #!${pkgs.bash}/bin/bash
+
+        set -e
+
+        SOURCE_DIR = "$1"
+        DEST_DIR = "$2"
+
+        mkdir -p "$DEST_DIR"
+
+        rsync -a --delete "$SOURCE_DIR/" "$DEST_DIR"
+      '';    
+    };
+  };
+}
