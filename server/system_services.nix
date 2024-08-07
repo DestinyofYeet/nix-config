b8e519622923d37f6dbb8f739d74df4985d21832
DestinyofYeet <olebend@gmail.com>
2024-08-07 14:37:46 +0200

    started splitting up services

--- server/system_services.nix
+++ server/system_services.nix
@@ -249,22 +249,6 @@ in {
     };
   };
 
-  systemd.services.add-replay-gain = {
-    description = "Automatically add replay gain to files";
-    wantedBy = [ "multi-user.target" ];
-
-    inherit (apps.add-replay-gain) enable;
-
-    serviceConfig = {
-      Type = "simple";
-      Restart = "always";
-      ExecStart = "${apps.add-replay-gain.workingDir}/add_replay_gain_to_files";
-      WorkingDir = apps.add-replay-gain.workingDir;
-      User = apps.user;
-      Group = apps.group;
-    };
-  };
-
   services.jellyfin = {
     inherit (apps.jellyfin) enable dataDir openFirewall;
     inherit (apps) user group;
