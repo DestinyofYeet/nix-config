0d63f0d398b082788c4fa053235e4f908f586c69
DestinyofYeet <olebend@gmail.com>
2024-08-07 13:15:32 +0200

    added mason config

---
+++ laptop/modules/neovim-cfg/lsp/mason.lua
@@ -0,0 +1,27 @@
+local mason = require("mason")
+ -- import mason-lspconfig
+local mason_lspconfig = require("mason-lspconfig")
+
+-- enable mason and configure icons
+mason.setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
+})
+
+mason_lspconfig.setup({
+  -- list of servers for mason to install
+  ensure_installed = {
+    "lua_ls",
+    "rust_analyzer",
+    "ast_grep",
+    "asm_lsp",
+    "clangd",
+  },
+  -- auto-install configured servers (with lspconfig)
+  automatic_installation = true, -- not the same as ensure_installed
+})
