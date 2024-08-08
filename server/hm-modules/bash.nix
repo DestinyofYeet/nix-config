8e4644004559f4cb874562f016a6ec97fe934a40
Ole Bendixen <olebend@gmail.com>
2024-08-08 21:06:18 +0200

    added bash config

---
+++ server/hm-modules/bash.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }: {
+
+  programs.bash = let  
+    rebuild-system = pkgs.writeShellScriptBin "rebuild-system" ''
+      set -e
+      cd /home/ole/nixos
+      ./build.sh
+      cd - > /dev/null
+    '';
+  in 
+  {
+    enable = true;
+
+    shellAliases = {
+      rebuild-system = "${rebuild-system}/bin/rebuild-system";
+    };
+  };
+}
--- server/hm-modules/default.nix
+++ server/hm-modules/default.nix
@@ -1,5 +1,6 @@
 { ... }: {
   imports = [
     ./btop.nix
+    ./bash.nix
   ];
 }
