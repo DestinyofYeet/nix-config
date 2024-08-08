b5f8340d79777a1217216a3bbdddb84db41f5e92
DestinyofYeet <olebend@gmail.com>
2024-08-08 19:13:52 +0200

    added create-email-alias

---
+++ laptop/scripts/create_email_alias.py
@@ -0,0 +1,35 @@
+import random
+import string
+import argparse
+import os
+
+def generate_alias(length: int, chars_to_use: list[str]) -> str:
+    password = ""
+    for _ in range(length):
+        password += random.choice(random.choice(chars_to_use))
+
+    return password
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Creates a new permanent email alias")
+
+    parser.add_argument("--random", action="store_true", dest="random", help="Generates a new alias to use")
+    parser.add_argument("given_alias", nargs="?", help="Uses the given alias to use")
+
+    parsed = parser.parse_args()
+
+    alias = parsed.given_alias
+
+    if parsed.random:
+        alias = generate_alias(20, [string.ascii_letters + string.digits])
+
+    if alias is None:
+        parser.print_help()
+        exit(1)
+
+    print(f"Alias: {alias}@uwuwhatsthis.de")    
+    os.system(f"stalwart-cli -u https://mx.uwuwhatsthis.de -c 'ole:&F8U%s&eWG#^qNcYg82UqzaSkcwNmsZ9ctj*sk4v' account add-email ole {alias}@uwuwhatsthis.de")
+
+if __name__ == '__main__':
+    main()
