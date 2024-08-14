229d4c25ab0f1c67f7d09100258e72cf0d1b9740
Ole Bendixen <olebend@gmail.com>
2024-08-14 16:10:24 +0200

    added innernet

---
+++ server/services/innernet.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:{
+  systemd.services.innernet-infra = {
+    description = "innernet client for infra";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network-online.target" "nss-lookup.target" ];
+    wants = [ "network-online.target" "nss-lookup.target" ];
+    serviceConfig = {
+      Restart = "always";
+      ExecStart =
+        "${pkgs.innernet}/bin/innernet up infra --daemon --interval 60 --no-write-hosts";
+    };
+  };
+}
