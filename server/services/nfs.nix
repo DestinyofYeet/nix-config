894f8376a50ac88f18e8954131a29d71a9a5970e
Ole Bendixen <olebend@gmail.com>
2024-08-15 18:00:22 +0200

    fixed nfs; allowed access to everyone;

--- server/services/nfs.nix
+++ server/services/nfs.nix
@@ -2,7 +2,7 @@
 let
   apps-id = "568"; 
 in {
-  fileSystems."/nfs/navidrome" = {
+  fileSystems."/export/navidrome" = {
     device = "/data/media/navidrome";
     options = [ "bind" ];
   };
@@ -10,8 +10,8 @@ in {
   services.nfs.server = {
     enable = true;
     exports = ''
-      /export             192.168.0.0/24(rw,fsid=0,no_subtree_check,all_squash,anonuid=${apps-id},anongid=${apps-id})
-      /export/navidrome   192.168.0.0/24(rw,nohide,insecure,no_subtree_check,all_squash,anonuid=${apps-id},anongid=${apps-id})
+      /export             0.0.0.0/1(rw,fsid=0,no_subtree_check,all_squash,anonuid=${apps-id},anongid=${apps-id})
+      /export/navidrome   0.0.0.0/1(rw,nohide,insecure,no_subtree_check,all_squash,anonuid=${apps-id},anongid=${apps-id})
     '';
   };
 }
