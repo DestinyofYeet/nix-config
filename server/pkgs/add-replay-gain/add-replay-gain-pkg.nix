5060f63394f30fab0bfcaa09f508d338334d31bf
Ole Bendixen <olebend@gmail.com>
2024-08-17 19:32:56 +0200

    updated add-replay-gain

--- server/pkgs/add-replay-gain/add-replay-gain-pkg.nix
+++ server/pkgs/add-replay-gain/add-replay-gain-pkg.nix
@@ -6,23 +6,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "add_replay_gain_to_files";
-  version = "1.2";
-
-  #src = fetchFromGitHub {
-  #  rev = "v${version}";
-
-  #  owner = "DestinyofYeet";
-  #  repo = "add_replay_gain";
-
-  #  hash = "sha256-hDOpIcz+OAWa+sH/kMqA4gcCdo7/aVf8pyslWhXenLY=";
-  #};
+  version = "1.3";
 
   src = fetchzip {
-    url = "https://github.com/DestinyofYeet/add_replay_gain/archive/00649d11166476d0557f98a3a0ef559ff7f31861.zip";
-    hash = "sha256-M/ykbeuBUSe41rfB0lucsHoSr6bKCSSVkOepY7i35Rk=";
+    url = "https://github.com/DestinyofYeet/add_replay_gain/archive/4954d457b5cf7bd0623735bfb7966d55ce1cb0be.zip";
+    hash = "sha256-TeUZxu8JbCsVjoUofL0fI6aPcfsDj13KL+UFN6QAr8A=";
   };
 
-  cargoHash = "sha256-WCo8S4BwEW8X4DxJbTbSiRBwUap89hWA9ivohyPEpqY=";
+  cargoHash = "sha256-KFMOF22nMW9iEf0z0mAcLdOz8YPaMvk7b2+C8bA2XAk=";
 
   meta = with lib; {
     description = "A tool to automatically add replay gain to mp3 and flac files";
