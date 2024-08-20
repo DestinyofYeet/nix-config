8f4489ca0518b250e602ef02096956e42eee0f7a
Ole Bendixen <olebend@gmail.com>
2024-08-20 12:43:39 +0200

    added ssh

---
+++ server/hm-modules/ssh.nix
@@ -0,0 +1,20 @@
+{ config, ...}:{
+  services.ssh-agent.enable = true;
+
+  programs.ssh.addKeysToAgent = true;
+
+  age.secrets = {
+    ssh-github = { file = ../secrets/ssh-github.age; };
+  };
+
+  home.file = {
+    "${config.home.homeDirectory}/.ssh/config" = {
+      text = ''
+        Host github.com
+          Hostname github.com
+          User git
+          IdentityFile ${config.age.secrets.ssh-github.path}
+      '';
+    };
+  };
+}
