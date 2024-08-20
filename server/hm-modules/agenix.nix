e7bfbea689fcaf97d2c13e95c09e791e9dc10249
Ole Bendixen <olebend@gmail.com>
2024-08-20 13:02:25 +0200

    added agenix

---
+++ server/hm-modules/agenix.nix
@@ -0,0 +1,8 @@
+{ ... }:{
+  age = {
+    identityPaths = [ "/home/ole/.ssh/id_ed25519" ];
+
+    secretsDir = "/home/ole/.agenix/agenix";
+    secretsMountPoint = "/home/ole/.agenix/agenix.d";
+  };
+}
