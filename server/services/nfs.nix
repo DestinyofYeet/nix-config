472fcd3c3ea8a6ccf7471a86d9f6a6cecfc572cf
Ole Bendixen <olebend@gmail.com>
2024-08-25 04:51:30 +0200

    changed access to unrestricted

--- server/services/nfs.nix
+++ server/services/nfs.nix
@@ -10,8 +10,8 @@ in {
   services.nfs.server = {
     enable = true;
     exports = ''
-      /export             0.0.0.0/1(rw,fsid=0,no_subtree_check,all_squash,anonuid=${apps-id},anongid=${apps-id})
-      /export/navidrome   0.0.0.0/1(rw,nohide,insecure,no_subtree_check,all_squash,anonuid=${apps-id},anongid=${apps-id})
+      /export             *(rw,fsid=0,no_subtree_check,all_squash,anonuid=${apps-id},anongid=${apps-id})
+      /export/navidrome   *(rw,nohide,insecure,no_subtree_check,all_squash,anonuid=${apps-id},anongid=${apps-id})
     '';
   };
 }
