c267097083aa32837e1b9fe231ce0158af240284
DestinyofYeet <olebend@gmail.com>
2024-08-28 13:29:52 +0200

    added everything as a safe directory

--- non-server/modules/git.nix
+++ non-server/modules/git.nix
@@ -1,8 +1,9 @@
 { ... }:
 {
   programs.git = {
-    attributes = {
-      safe.directory = "/drives/programming-Stuff/rust/clean_unused_files/"
-    };
+    extraConfig = ''
+      [safe] 
+        directory = *;
+    '';
   };
 }
