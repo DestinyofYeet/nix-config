c31ae863339105348aa51ea3e046be613a55cac0
DestinyofYeet <olebend@gmail.com>
2024-09-10 11:26:54 +0200

    modified prowlarr to use configs dir

--- server/services/prowlarr.nix
+++ server/services/prowlarr.nix
@@ -1,5 +1,17 @@
-{ ... }:{
-  services.prowlarr = {
+{ pkgs, config, ... }:{
+
+  systemd.services.prowlarr = {
+    description = "Prowlarr";
+    wantedBy = [ "multi-user.target" ];
+
     enable = true;
+    serviceConfig = {
+      Type = "simple";
+      Restart = "always";
+      User = config.serviceSettings.user;
+      Group = config.serviceSettings.group;
+      ExecStart =
+        "${pkgs.prowlarr}/bin/Prowlarr -nobrowser -data=${config.serviceSettings.paths.configs}/prowlarr";
+    };
   };
 }
