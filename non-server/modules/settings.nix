3621fb96da79c778399722df2fe95fe2cba3c1ca
DestinyofYeet <olebend@gmail.com>
2024-09-26 14:30:31 +0200

    updated stable-pkgs

--- non-server/modules/settings.nix
+++ non-server/modules/settings.nix
@@ -7,8 +7,8 @@
 let 
 
   nixos-stable = builtins.fetchTarball {
-    url = "https://github.com/nixos/nixpkgs/tarball/44a71ff39c182edaf25a7ace5c9454e7cba2c658";
-    sha256 = "14w93hcmaa2jwg7ql2gsnh1s982smc599irk4ykkskg537v46n25";
+    url = "https://github.com/nixos/nixpkgs/tarball/30439d93eb8b19861ccbe3e581abf97bdc91b093";
+    sha256 = "1fa67745sf5f9mdz4slkk814dzn93bi72k25wafi21w74mw43id3";  # done with nix-prefetch-url --unpack
   };
   pkgs-stable = import (nixos-stable) { system = "x86_64-linux"; };
 in {
