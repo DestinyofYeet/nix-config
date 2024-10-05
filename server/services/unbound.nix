732e87b8494ad08e7f2dca3cf3a8f86a67053f08
DestinyofYeet <olebend@gmail.com>
2024-10-05 14:07:44 +0200

    attempts to use auth zones; fails

--- server/services/unbound.nix
+++ server/services/unbound.nix
@@ -1,16 +1,36 @@
 {
   config,
+  pkgs,
   ...
 }:
 
 let
   root-domain = "nix-server.infra.wg";
   ip = "192.168.0.250";
-  build-subdomains = sub-domains:
-    (map (sub-domain:  "\"${sub-domain}.${root-domain}. IN A ${ip}\"") sub-domains);
+  build-subdomains = build-domain: build-ip: sub-domains:
+    (map (sub-domain:  (wrap-string "${sub-domain}.${build-domain}. IN A ${build-ip}")) sub-domains);
+
+  wrap-string = string:
+    "\"${string}\"";
+
+  zone-file = pkgs.writeText "local.ole.blue.zone" ''
+    $TTL 86400
+
+    @  IN  SOA ns.local.ole.blue ole@uwuwhatsthis.de. (
+      2024100501   ; YYYYMMDDNN Serial
+      3600         ; Refresh
+      1800         ; Retry
+      1209600      ; Expire
+      86400        ; Minimum TTL
+    ) IN NS ns.local.ole.blue.
+
+    ns.local.ole.blue. IN A 192.168.0.250
+    *.local.ole.blue IN A 192.168.0.250
+  '';
 in {
   services.unbound = {
     enable = true;
+    checkconf = true;
     resolveLocalQueries = true;
     settings = {
       server = {
@@ -37,7 +57,7 @@ in {
         # needs quotes
         local-data = [
           "\"${root-domain}. IN A ${ip}\""
-        ] ++ build-subdomains [
+        ] ++ build-subdomains "nix-server.infra.wg" "192.168.0.250" [
           "jellyfin"
           "firefly"
           "cache"
@@ -49,6 +69,23 @@ in {
           "syncthing"
         ];
       };
+
+      # I want the unbound server to have a local storage of the zone data incase the internet goes out
+      # stub-zone = [
+      #   {
+      #     name = wrap-string "local.ole.blue.";
+      #     stub-addr = "192.168.0.250";
+      #     # zonefile = wrap-string "${zone-file}";
+      #   }
+      # ];
+
+      # auth-zone = [
+      #   {
+      #     name = wrap-string "local.ole.blue";
+      #     zonefile = wrap-string "${zone-file}";
+      #   }
+      # ];
+
       forward-zone = [
         {
           name = ".";
