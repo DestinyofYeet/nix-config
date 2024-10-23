3a70f6b5327c2cf433c23dda09124e96bb3fe20c
DestinyofYeet <olebend@gmail.com>
2024-10-23 20:17:58 +0200

    fixes nix-gc

--- baseline/configuration.nix
+++ baseline/configuration.nix
@@ -1,17 +1,25 @@
-{ ... }: {
+{ lib, config, ... }: {
 
   # automatically collect garbage
-  nix.gc = {
-    automatic = true;
-    persistent = true;
+  config = {
+    nix.gc = {
+      automatic = true;
+      persistent = true;
 
-    options = "--delete-older-than +10";
-    dates = "05:00:00"; 
+      options = "--delete-generations +10";
+      dates = "05:00:00"; 
+    };
+
+    # optimise nix-store with deduplication
+    nix.settings.auto-optimise-store = true;
+
+    nix.settings.max-jobs = "auto";
   };
 
-  # optimise nix-store with deduplication
-  nix.settings.auto-optimise-store = true;
-
-  nix.settings.max-jobs = "auto";
+  config.systemd.services.nix-gc = lib.mkIf config.nix.enable {
+    script = lib.mkForce ''
+      exec ${config.nix.package.out}/bin/nix-env ${config.nix.gc.options}
+      exec ${config.nix.package.out}/bin/nix-collect-garbage'';
+  };
 }
 
