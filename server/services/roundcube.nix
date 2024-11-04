1a9019c21d6d59d32faf1ab29c1fe199680131a2
DestinyofYeet <olebend@gmail.com>
2024-11-04 21:31:59 +0100

    fixes roundcube

--- server/services/roundcube.nix
+++ server/services/roundcube.nix
@@ -7,6 +7,8 @@
     hostName = "webmail.local.ole.blue";
     extraConfig = ''
       $config['smtp_server'] = "tls://mx.uwuwhatsthis.de";
+      $config['default_host'] = "tls://mx.uwuwhatsthis.de";
+      $config['default_port'] = 993;
       $config['smtp_user'] = "%u";
       $config['smtp_pass'] = "%p";
     '';
