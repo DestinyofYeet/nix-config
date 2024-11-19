0d13504570fb371d520e38ef363fd5773d233f78
DestinyofYeet <olebend@gmail.com>
2024-11-19 10:39:19 +0100

    changes import script

--- server/services/firefly-iii-importer.nix
+++ server/services/firefly-iii-importer.nix
@@ -15,15 +15,8 @@
     };
 
     script = ''
-      ${pkgs.curl}/bin/curl -X GET "http://localhost:7070/?automate=true&config=targobank.json" 
-      retval=$?
-      if [ $retval -ne 0 ]; then
-        content="Error importing data" 
-      else
-        content="Successfully imported data"
-      fi
-
-      ${config.serviceScripts.send-email}/bin/send-email --subject "Imported Firefly III data" --content $content --password_file ${config.age.secrets.send-email-pw.path}
+      output = $(${pkgs.curl}/bin/curl -X GET "http://localhost:7070/?automate=true&config=targobank.json")
+      ${config.serviceScripts.send-email}/bin/send-email --subject "Imported Firefly III data" --content $output --password_file ${config.age.secrets.send-email-pw.path}
     '';
   };
 
