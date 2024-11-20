17929ba1c180187c0c35f20ce445b6c488893cdc
DestinyofYeet <olebend@gmail.com>
2024-11-20 01:41:11 +0100

    attempts to fix sonarr issues

--- server/services/sonarr.nix
+++ server/services/sonarr.nix
@@ -12,10 +12,19 @@
         proxyPass = "http://localhost:8989";
         extraConfig = ''
           # Headers for WebSocket support
-          proxy_set_header Upgrade $http_upgrade;
-          proxy_set_header Connection "upgrade";
+          proxy_set_header   Host $host;
+          proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header   X-Forwarded-Host $host;
+          proxy_set_header   X-Forwarded-Proto $scheme;
+          proxy_set_header   Upgrade $http_upgrade;
+          proxy_set_header   Connection $http_connection;
+
+          proxy_redirect     off;
+          proxy_http_version 1.1;
         '';
       };
+
+      
     };
   in {
     
