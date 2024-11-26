d6458f4b7c0f5f849e4b90baad8cfb29d0af9655
DestinyofYeet <olebend@gmail.com>
2024-11-26 01:41:58 +0100

    adds laptop

--- server/teapot/users.nix
+++ server/teapot/users.nix
@@ -5,6 +5,7 @@
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILDTPAyrp7iPzStIEQnGifJQ6JsYtD2gbE93GchJnfND ole@main"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkPxsSPtso+b6Cwn7oxpwHxjzauASSlM1YZaEI6LP+t ole@wattson"
     ];
 
     initialPassword = "changeme";
