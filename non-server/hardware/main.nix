b05a4bc220693309161d42643518d8d2f210d52c
DestinyofYeet <olebend@gmail.com>
2024-12-21 00:37:17 +0100

    fixes SteamVR

--- non-server/hardware/main.nix
+++ non-server/hardware/main.nix
@@ -64,7 +64,8 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  hardware.amdgpu.amdvlk.enable = true;
+  # fixes SteamVR for some reason
+  # hardware.amdgpu.amdvlk.enable = true;
 
   fileSystems = {
     "/drives/navidrome" = {
