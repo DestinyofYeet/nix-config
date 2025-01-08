9d939a253fb32f481b97c9f863a5afbe2a02b85b
DestinyofYeet <olebend@gmail.com>
2025-01-09 00:29:46 +0100

    vpn test

--- server/nix-server/services/vpn.nix
+++ server/nix-server/services/vpn.nix
@@ -2,11 +2,7 @@
   flake,
   config,
   ...
-}:
-let
-  vpn_port = flake.nixosConfigurations.teapot.config.networking.wireguard.interfaces.wg0.listenPort;
-in
-{
+}:{
   age.secrets = {
     wireguard-vpn-priv-key.file = ../secrets/wireguard-vpn-priv-key.age;
   };
@@ -14,7 +10,6 @@ in
   networking.wireguard.interfaces = {
     wg0 = {
       ips = [ "10.100.0.4/32" ];
-      # listenPort = vpn_port;
 
       privateKeyFile = config.age.secrets.wireguard-vpn-priv-key.path;
 
@@ -23,9 +18,9 @@ in
           # teapot
           publicKey = "cLPAuu+Pu0nTBenl+ezZyjtVNqP3WYBzKM8BPYQ4Jh8=";
 
-          allowedIPs = [ "10.100.0.0/24" ];
+          allowedIPs = [ "10.100.0.0/24" "10.100.0.1" ];
 
-          endpoint = "5.83.152.153:${toString vpn_port}";
+          endpoint = "5.83.152.153:53";
 
           persistentKeepalive = 25;
         }
