f70a7a52115438ef2fe52f7ea5f8da7ec5248af5
DestinyofYeet <olebend@gmail.com>
2025-01-13 20:40:05 +0100

    sets extraConf to nix-server value

--- server/teapot/services/home-proxy.nix
+++ server/teapot/services/home-proxy.nix
@@ -18,7 +18,7 @@ let
     "cloud.local.ole.blue"
   ];
 
-  getPort = url: (builtins.elemAt (lib.splitString ":" (lib.last (lib.splitString "://" url))) 1);
+  # getPort = url: (builtins.elemAt (lib.splitString ":" (lib.last (lib.splitString "://" url))) 1);
 
   filteredHosts = lib.filterAttrs (name: value: !(lib.elem name blacklistedHosts)) (
     lib.filterAttrs (name: value: (lib.hasSuffix ".local.ole.blue" name)) hosts
@@ -29,7 +29,6 @@ let
     forceSSL = true;
     enableACME = true;
     locations."/" = {
-      # proxyPass = "http://10.100.0.4:${getPort value.locations."/".proxyPass}";
       proxyPass = "https://local.ole.blue";
       extraConfig = value.locations."/".extraConfig;
     };
@@ -44,21 +43,23 @@ in
       forceSSL = true;
       enableACME = true;
 
-      extraConfig = ''
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+      extraConfig = flake.nixosConfigurations.nix-server.config.services.nginx.virtualHosts."cloud.local.ole.blue".extraConfig;
 
-        add_header X-Content-Type-Options nosniff;
-        add_header Strict-Transport-Security "max-age=15552000; includeSubDomains";
-        add_header X-XSS-Protection "1; mode=block";
-        add_header X-Robots-Tag "noindex, nofollow";
-        add_header X-Frame-Options "SAMEORIGIN";
-        add_header Referrer-Policy "no-referrer";
+      # extraConfig = ''
+      #   proxy_set_header Host $host;
+      #   proxy_set_header X-Real-IP $remote_addr;
+      #   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      #   proxy_set_header X-Forwarded-Proto $scheme;
 
-        client_max_body_size ${flake.nixosConfigurations.nix-server.config.services.nextcloud.maxUploadSize};
-      '';
+      #   add_header X-Content-Type-Options nosniff;
+      #   add_header Strict-Transport-Security "max-age=15552000; includeSubDomains";
+      #   add_header X-XSS-Protection "1; mode=block";
+      #   add_header X-Robots-Tag "noindex, nofollow";
+      #   add_header X-Frame-Options "SAMEORIGIN";
+      #   add_header Referrer-Policy "no-referrer";
+
+      #   client_max_body_size ${flake.nixosConfigurations.nix-server.config.services.nextcloud.maxUploadSize};
+      # '';
     };
   };
 }
