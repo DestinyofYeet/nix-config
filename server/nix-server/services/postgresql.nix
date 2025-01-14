3506e37b21c0d1a3e175bfb836ccae7e06d8b8e7
DestinyofYeet <ole@ole.blue>
2025-01-14 18:47:50 +0100

    nix fmt

--- server/nix-server/services/postgresql.nix
+++ server/nix-server/services/postgresql.nix
@@ -18,44 +18,48 @@
 
     dataDir = "${lib.custom.settings.${config.networking.hostName}.paths.data}/postgresql";
 
-    ensureUsers = let
-      build-ensureUsers = names: [
-        (map (x: {name = x; ensureDBOwnership = true;}) names)
+    ensureUsers =
+      let
+        build-ensureUsers = names: [
+          (map (x: {
+            name = x;
+            ensureDBOwnership = true;
+          }) names)
+        ];
+      in
+      # build-ensureUsers [
+      #   "hydra"
+      #   # config.services.wiki-js.settings.db.user
+      #   # config.services.nextcloud.config.dbuser
+      #   # flake.nixosConfigurations.teapot.config.services.gitea.database.user
+      # ];
+      [
+        {
+          name = "hydra";
+          ensureDBOwnership = true;
+        }
+        {
+          name = "${config.services.wiki-js.settings.db.user}";
+          ensureDBOwnership = true;
+        }
+        {
+          name = config.services.nextcloud.config.dbuser;
+          ensureDBOwnership = true;
+        }
+        {
+          name = flake.nixosConfigurations.teapot.config.services.gitea.database.user;
+          ensureDBOwnership = true;
+        }
+        # {
+        #   name = flake.nixosConfigurations.teapot.config.services.roundcube.database.username;
+        #   ensureDBOwnership = true;
+        # }
       ];
-    in
-    # build-ensureUsers [
-    #   "hydra"
-    #   # config.services.wiki-js.settings.db.user
-    #   # config.services.nextcloud.config.dbuser
-    #   # flake.nixosConfigurations.teapot.config.services.gitea.database.user
-    # ];
-    [
-      {
-        name = "hydra";
-        ensureDBOwnership = true;
-      }
-      {
-        name = "${config.services.wiki-js.settings.db.user}";
-        ensureDBOwnership = true;
-      }
-      {
-        name = config.services.nextcloud.config.dbuser;
-        ensureDBOwnership = true;
-      }
-      {
-        name = flake.nixosConfigurations.teapot.config.services.gitea.database.user;
-        ensureDBOwnership = true;
-      }
-      # {
-      #   name = flake.nixosConfigurations.teapot.config.services.roundcube.database.username;
-      #   ensureDBOwnership = true;
-      # }
-    ];
 
     authentication = ''
       host all all 10.100.0.1/32 md5
     '';
-  
+
     ensureDatabases = [
       "hydra"
       config.services.wiki-js.settings.db.db
