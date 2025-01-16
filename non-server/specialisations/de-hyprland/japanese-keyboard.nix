daa11b5504882dfbb5cef3765963af351eb35500
DestinyofYeet <ole@ole.blue>
2025-01-16 15:03:00 +0100

    suppresses env warning

--- non-server/specialisations/de-hyprland/japanese-keyboard.nix
+++ non-server/specialisations/de-hyprland/japanese-keyboard.nix
@@ -7,11 +7,14 @@
     enable = true;
     type = "fcitx5";
 
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-      fcitx5-configtool
-    ];
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+        fcitx5-configtool
+      ];
+    };
   };
 
   # environment.systemPackages = with pkgs; [
