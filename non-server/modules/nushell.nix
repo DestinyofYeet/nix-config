be15bbbea89c8b5247a6e2d434d353eaff36ca10
DestinyofYeet <ole@ole.blue>
2025-01-31 13:08:44 +0100

    enables history isolation

--- non-server/modules/nushell.nix
+++ non-server/modules/nushell.nix
@@ -3,8 +3,7 @@
   pkgs,
   current-specialisation,
   ...
-}:
-{
+}: {
   programs.nushell = {
     enable = true;
 
@@ -15,6 +14,12 @@
 
       $env.config = {
         show_banner: false
+      }
+
+      $env.config.history = {
+        file_format: "sqlite"
+        sync_on_enter: true
+        isolation: true
       }
 
       def --wrapped build-system [ function: string, ...args ] {
@@ -36,18 +41,16 @@
   };
 
   # nushell doesn't include symlinks
-  home.activation =
-    let
-      source = builtins.fetchurl {
-        url = "https://raw.githubusercontent.com/nushell/nu_scripts/ba13f5ca600ee537880674e065ad237a89161e97/modules/background_task/task.nu";
-        sha256 = "05lafp08fvv22kb6k2npg0g25jpwdz1myz3aaqwslxcab4ivfbgw";
-      };
-    in
-    {
-      write-task-file = ''
-        ${lib.custom.update-needed-content-file}/bin/update-needed-content-file "${source}" "/home/ole/.config/nushell/scripts/task.nu"
-      '';
+  home.activation = let
+    source = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/nushell/nu_scripts/ba13f5ca600ee537880674e065ad237a89161e97/modules/background_task/task.nu";
+      sha256 = "05lafp08fvv22kb6k2npg0g25jpwdz1myz3aaqwslxcab4ivfbgw";
     };
+  in {
+    write-task-file = ''
+      ${lib.custom.update-needed-content-file}/bin/update-needed-content-file "${source}" "/home/ole/.config/nushell/scripts/task.nu"
+    '';
+  };
 
   systemd.user.services = {
     pueued = {
@@ -56,7 +59,7 @@
       };
 
       Install = {
-        WantedBy = [ "graphical-session.target" ];
+        WantedBy = ["graphical-session.target"];
       };
 
       Service = {
