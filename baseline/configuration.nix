b0d7cc2bb310408ffd6d81610f0c6767d4934d36
DestinyofYeet <ole@ole.blue>
2025-02-14 22:27:44 +0100

    fix(nix-channels): Sync flake nixpkgs with channel nixpkgs

--- baseline/configuration.nix
+++ baseline/configuration.nix
@@ -2,6 +2,7 @@
   lib,
   config,
   pkgs,
+  inputs,
   ...
 }: {
   imports = [
@@ -42,4 +43,8 @@
   };
 
   boot.tmp.cleanOnBoot = true;
+
+  # match nix-channels (nix-shell) with nix flake input
+  nix.nixPath = ["nixpkgs=/etc/channels/nixpkgs" "nixos-config=/etc/nixos/configuration.nix" "/nix/var/nix/profiles/per-user/root/channels"];
+  environment.etc."channels/nixpkgs".source = inputs.nixpkgs.outPath;
 }
