3a927687851e13290d1853a725f6db6d2f765de7
DestinyofYeet <ole@ole.blue>
2025-02-14 20:55:25 +0100

    feat(build): Build.sh now uses a nix-shell

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p nix-output-monitor
 
 sudo -v
-sudo nixos-rebuild switch --flake .# $@ --log-format internal-json -v |& nom --json
+sudo nixos-rebuild $1 --flake .# ${@:2} --log-format internal-json -v |& nom --json
