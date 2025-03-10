666c5d771dc249aff542678fac1c95ddfb1b5933
DestinyofYeet <ole@ole.blue>
2025-03-10 20:18:54 +0100

    feat(servers/nix-server/syncthing): Increases syncthing connection amount

--- server/nix-server/services/syncthing.nix
+++ server/nix-server/services/syncthing.nix
@@ -1,11 +1,12 @@
-{ config, lib, ... }:
-{
+{ config, lib, ... }: {
   services.syncthing = {
     enable = true;
     settings = {
       folders = {
         camera = {
-          path = "${lib.custom.settings.${config.networking.hostName}.paths.data}/photos/handy/Camera";
+          path = "${
+              lib.custom.settings.${config.networking.hostName}.paths.data
+            }/photos/handy/Camera";
           label = "Camera";
           id = "camera";
 
@@ -13,7 +14,9 @@
         };
 
         default = {
-          path = "${lib.custom.settings.${config.networking.hostName}.paths.data}/syncthing/default-folder";
+          path = "${
+              lib.custom.settings.${config.networking.hostName}.paths.data
+            }/syncthing/default-folder";
           label = "Default Folder";
           id = "default";
         };
@@ -22,35 +25,37 @@
       devices = {
         handy = {
           name = "Handy";
-          id = lib.custom.settings.${config.networking.hostName}.secrets.syncthing.handy.id;
+          id =
+            lib.custom.settings.${config.networking.hostName}.secrets.syncthing.handy.id;
+          numconnections = 10;
         };
       };
 
       gui = {
         user = "admin";
-        password = "$2b$12$6/o5B5F0.L14z4AAa2beju7zKXELfXD/toTtH1iH8xQ4A7nX3vQE6";
+        password =
+          "$2b$12$6/o5B5F0.L14z4AAa2beju7zKXELfXD/toTtH1iH8xQ4A7nX3vQE6";
       };
     };
 
     guiAddress = "127.0.0.1:8384";
 
-    dataDir = "${lib.custom.settings.${config.networking.hostName}.paths.data}/syncthing";
+    dataDir = "${
+        lib.custom.settings.${config.networking.hostName}.paths.data
+      }/syncthing";
 
     inherit (lib.custom.settings.${config.networking.hostName}) user group;
   };
 
-  services.nginx =
-    let
-      default-config = {
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8384";
-        };
-      };
-    in
-    {
-      virtualHosts = {
-        "syncthing.local.ole.blue" =
-          lib.custom.settings.${config.networking.hostName}.nginx-local-ssl // default-config;
-      };
+  services.nginx = let
+    default-config = {
+      locations."/" = { proxyPass = "http://127.0.0.1:8384"; };
     };
+  in {
+    virtualHosts = {
+      "syncthing.local.ole.blue" =
+        lib.custom.settings.${config.networking.hostName}.nginx-local-ssl
+        // default-config;
+    };
+  };
 }
