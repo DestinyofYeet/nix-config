4bc0211ea6e392a7d5a0e7abb7e2bd1f4ade700f
DestinyofYeet <ole@ole.blue>
2025-03-12 20:52:56 +0100

    fix(servers/nix-server/deluge): Fixes namespace dependency

--- server/nix-server/services/deluge.nix
+++ server/nix-server/services/deluge.nix
@@ -1,5 +1,4 @@
-{ config, lib, ... }:
-{
+{ config, lib, ... }: {
 
   age.secrets = {
     airvpn-deluge = {
@@ -7,9 +6,7 @@
       path = "/etc/wireguard/wg1.conf";
     };
 
-    deluge-password-file = {
-      file = ../secrets/deluge-password-file.age;
-    };
+    deluge-password-file = { file = ../secrets/deluge-password-file.age; };
   };
 
   services.deluge = {
@@ -20,17 +17,18 @@
     inherit (lib.custom.settings.${config.networking.hostName}) user group;
   };
 
-  systemd.services.deluged.serviceConfig.NetworkNamespacePath = "/var/run/netns/deluge";
+  systemd.services.deluged.serviceConfig.NetworkNamespacePath =
+    "/var/run/netns/deluge";
 
-  systemd.services.deluged.serviceConfig.ExecStart =
-    lib.mkForce "${config.services.deluge.package}/bin/deluged --do-not-daemonize --config /mnt/data/configs/deluge/.config/deluge --ui-interface 10.1.2.1";
+  systemd.services.deluged.serviceConfig.ExecStart = lib.mkForce
+    "${config.services.deluge.package}/bin/deluged --do-not-daemonize --config /mnt/data/configs/deluge/.config/deluge --ui-interface 10.1.2.1";
 
   customLibs.networkNamespaces = {
     enable = true;
     spaces = {
       deluge = {
         enable = true;
-        upholdsServices = [ "deluged.service" ];
+        upholdsServices = [ "deluged.service" "delugeweb.service" ];
         networkIpIn = "10.1.2.1";
         networkIpOut = "10.1.2.2";
         wireguardConfigPath = config.age.secrets.airvpn-deluge.path;
@@ -43,24 +41,25 @@
   };
 
   services.nginx.virtualHosts = {
-    "deluge.local.ole.blue" = lib.custom.settings.${config.networking.hostName}.nginx-local-ssl // {
-      locations."/" = {
-        proxyPass = "http://localhost:8112";
+    "deluge.local.ole.blue" =
+      lib.custom.settings.${config.networking.hostName}.nginx-local-ssl // {
+        locations."/" = {
+          proxyPass = "http://localhost:8112";
 
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
 
-          # Prevent gzip encoding issues
-          proxy_set_header Accept-Encoding "";
+            # Prevent gzip encoding issues
+            proxy_set_header Accept-Encoding "";
 
-          # If necessary, disable buffer to get immediate response from upstream
-          proxy_buffering off;
-        '';
+            # If necessary, disable buffer to get immediate response from upstream
+            proxy_buffering off;
+          '';
+        };
       };
-    };
   };
 
   services.prometheus.exporters.deluge = {
