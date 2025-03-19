122a6cefcdf1a7879d76d921d9dd981a5c17ee7c
DestinyofYeet <ole@ole.blue>
2025-03-20 00:51:08 +0100

    chore(non-servers/helix): Adds nix paths

--- non-server/modules/helix.nix
+++ non-server/modules/helix.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 let
   typst-watch-script = pkgs.writeShellScript "watch-typst.sh" ''
-    dir=$(mktemp -d)
-    _=$(typst watch "$1" "$dir/tmp.pdf" & echo $! > "$dir/pid")&
+    dir=$(${pkgs.mktemp}/bin/mktemp -d)
+    _=$(${pkgs.typst}/bin/typst watch "$1" "$dir/tmp.pdf" & echo $! > "$dir/pid")&
     until [ -f "$dir/tmp.pdf" ]
     do
       sleep 0.5
     done
-    pid=$(cat "$dir/pid")
-    zathura "$dir/tmp.pdf"
+    pid=$(${pkgs.coreutils}/bin/cat "$dir/pid")
+    ${pkgs.zathura}/bin/zathura "$dir/tmp.pdf"
     kill "$pid"
     rm -fr "$dir"
   '';
