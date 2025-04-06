8c44b11399656e4a787900b72e7b0e8dffd0a63d
DestinyofYeet <ole@ole.blue>
2025-04-06 20:48:25 +0200

    feat(servers/nix-server/smartd): Improves email notfications

--- server/nix-server/services/smartd.nix
+++ server/nix-server/services/smartd.nix
@@ -1,6 +1,8 @@
 { ... }: {
   services.smartd = {
     # enabled through baseline
+    # will start a short self-test every day between 2-3am, and an extended self test weekly on Saturdays between 3-4am
+    defaults.autodetected = "-a -s (S/../.././02|L/../../6/03)";
     notifications = {
       # test = true;
       mail = {
