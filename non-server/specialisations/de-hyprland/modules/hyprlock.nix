7de7638ad57fdd5aea85a20ab3a5effbc3b7bf0e
DestinyofYeet <ole@ole.blue>
2025-04-08 02:39:06 +0200

    feat(non-server/specialisation/hyprland): Changes position to percentages

--- non-server/specialisations/de-hyprland/modules/hyprlock.nix
+++ non-server/specialisations/de-hyprland/modules/hyprlock.nix
@@ -1,5 +1,4 @@
-{ lib, ... }:
-{
+{ lib, ... }: {
   programs.hyprlock = {
     enable = true;
 
@@ -11,36 +10,35 @@
         no_fade_in = true;
       };
 
-      background = [ { path = "${lib.custom.settings.non-server.lock-screen}"; } ];
+      background =
+        [{ path = "${lib.custom.settings.non-server.lock-screen}"; }];
 
-      input-field = [
-        {
-          monitor = "";
+      input-field = [{
+        monitor = "";
 
-          size = "300, 50";
-          outline_thickness = 1;
+        size = "300, 50";
+        outline_thickness = 1;
 
-          font_color = "rgb(b6c4ff)";
-          outer_color = "rgba(180, 180, 180, 0.5)";
-          inner_color = "rgba(200, 200, 200, 0.1)";
-          check_color = "rgba(247, 193, 19, 0.5)";
-          fail_color = "rgba(255, 106, 134, 0.5)";
+        font_color = "rgb(b6c4ff)";
+        outer_color = "rgba(180, 180, 180, 0.5)";
+        inner_color = "rgba(200, 200, 200, 0.1)";
+        check_color = "rgba(247, 193, 19, 0.5)";
+        fail_color = "rgba(255, 106, 134, 0.5)";
 
-          fade_on_empty = false;
-          placeholder_text = "Enter Password";
+        fade_on_empty = false;
+        placeholder_text = "Enter Password";
 
-          dots_spacing = 0.2;
-          dots_center = true;
-          dots_fade_time = 100;
+        dots_spacing = 0.2;
+        dots_center = true;
+        dots_fade_time = 100;
 
-          shadow_color = "rgba(0, 0, 0, 0.1)";
-          shadow_size = 7;
-          shadow_passes = 1;
+        shadow_color = "rgba(0, 0, 0, 0.1)";
+        shadow_size = 7;
+        shadow_passes = 1;
 
-          valign = "center";
-          halign = "center";
-        }
-      ];
+        valign = "center";
+        halign = "center";
+      }];
 
       label = [
         {
@@ -49,7 +47,7 @@
           font_size = 150;
           color = "rgb(b6c4ff)";
 
-          position = "0, -200";
+          position = "0, -25%";
 
           halign = "center";
           valign = "top";
@@ -60,7 +58,7 @@
           font_size = 20;
           color = "rgb(b6c4ff)";
 
-          position = "0, -200";
+          position = "0, -23%";
           halign = "center";
           valign = "top";
         }
