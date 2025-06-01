7ed2bf4d581ecbe9510edd0f372c8389dfdc094e
DestinyofYeet <ole@ole.blue>
2025-06-01 23:37:17 +0200

    fix(servers/nix-server/services): Fixes radarr results viewing

--- server/nix-server/services/radarr.nix
+++ server/nix-server/services/radarr.nix
@@ -1,22 +1,19 @@
-{
-  config,
-  lib,
-  ...
-}:
-{
+{ config, lib, ... }: {
   services.radarr = {
     enable = true;
 
-    dataDir = "${lib.custom.settings.${config.networking.hostName}.paths.configs}/radarr";
+    dataDir = "${
+        lib.custom.settings.${config.networking.hostName}.paths.configs
+      }/radarr";
 
     inherit (lib.custom.settings.${config.networking.hostName}) user group;
   };
 
   services.nginx.virtualHosts."radarr.local.ole.blue" =
-    lib.custom.settings.${config.networking.hostName}.nginx-local-ssl
-    // {
+    lib.custom.settings.${config.networking.hostName}.nginx-local-ssl // {
       locations."/" = {
         proxyPass = "http://localhost:7878";
+        proxyWebsockets = true;
         extraConfig = ''
           proxy_set_header   Host $host;
           proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -26,7 +23,12 @@
           proxy_set_header   Connection $http_connection;
 
           proxy_redirect     off;
-          proxy_http_version 1.1;
+
+          # needed, otherwise results won't load
+          send_timeout 100m;
+          proxy_connect_timeout 1800;
+          proxy_send_timeout 1800;
+          proxy_read_timeout 100m;
         '';
       };
     };
