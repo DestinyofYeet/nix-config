452b8217cb8068a24964cb4c844bc90d3ea8b09e
DestinyofYeet <ole@ole.blue>
2025-08-31 22:03:45 +0200

    chore(ssh-agent): Fix ssh-agent

--- non-server/specialisations/de-hyprland/modules/gnome-keyring.nix
+++ non-server/specialisations/de-hyprland/modules/gnome-keyring.nix
@@ -1,6 +1,4 @@
-{ ... }:
-{
-  services.gnome-keyring = {
-    enable = true;
-  };
+{ ... }: {
+  services.gnome-keyring = { enable = true; };
+
 }
--- non-server/specialisations/de-hyprland/programs.nix
+++ non-server/specialisations/de-hyprland/programs.nix
@@ -1,11 +1,7 @@
-{
-  pkgs,
-  ...
-}:
-{
+{ pkgs, lib, ... }: {
   programs.iio-hyprland.enable = true;
+  # disables ssh-agent because it just tries all ssh-keys on a server and then gets disconnected because it tried the wrong ones too often
+  services.gnome.gcr-ssh-agent.enable = lib.mkForce false;
 
-  environment.systemPackages = with pkgs; [
-    wvkbd
-  ];
+  environment.systemPackages = with pkgs; [ wvkbd ];
 }
