d957a8a3f6d695117bcb9a27254eaa0cc8f49be7
DestinyofYeet <ole@ole.blue>
2025-11-18 00:07:55 +0100

    fix(nebula): nix fmt

--- custom/nebula/default.nix
+++ custom/nebula/default.nix
@@ -4,22 +4,17 @@ let
 
   machines = import ./machines.nix;
 
-  genStaticHostMap =
-    machines:
-    builtins.listToAttrs (
-      map (entry: {
-        name = entry.value.ip;
-        value = entry.value.external_ips;
-      }) (builtins.filter (attr: attr.value.lighthouse or false) (lib.attrsToList machines))
-    );
+  genStaticHostMap = machines:
+    builtins.listToAttrs (map (entry: {
+      name = entry.value.ip;
+      value = entry.value.external_ips;
+    }) (builtins.filter (attr: attr.value.lighthouse or false)
+      (lib.attrsToList machines)));
 
-  getLighthouses =
-    machines:
-    lib.mapAttrsToList (_v: v: v.ip) (
-      lib.filterAttrs (name: value: value.lighthouse or false) machines
-    );
-in
-rec {
+  getLighthouses = machines:
+    lib.mapAttrsToList (_v: v: v.ip)
+    (lib.filterAttrs (name: value: value.lighthouse or false) machines);
+in rec {
   yeet = {
     staticHostMap = genStaticHostMap machines;
     lightHouses = getLighthouses machines;
@@ -27,14 +22,12 @@ rec {
     hosts = import ./machines.nix;
   };
 
-  getConfig =
-    lib: config:
+  getConfig = lib: config:
     let
       currentName = config.networking.hostName;
       currentNode = yeet.hosts.${currentName};
       isLightHouse = currentNode.lighthouse or false;
-    in
-    {
+    in {
       age.secrets = {
         "nebula-${currentName}-priv" = rec {
           file = currentNode.privKeyFile;
@@ -49,8 +42,8 @@ rec {
         cert = currentNode.publicKeyFile;
         ca = ./ca.crt;
 
-        staticHostMap = lib.mkIf (!(isLightHouse)) yeet.staticHostMap;
-        lighthouses = lib.mkIf (!(isLightHouse)) yeet.lightHouses;
+        staticHostMap = lib.mkIf (!isLightHouse) yeet.staticHostMap;
+        lighthouses = lib.mkIf (!isLightHouse) yeet.lightHouses;
 
         isLighthouse = isLightHouse;
         listen = lib.mkIf isLightHouse {
@@ -58,18 +51,14 @@ rec {
           port = 4242;
         };
 
-        settings = {
-          punchy.punch = lib.mkIf (!isLightHouse) true;
-        };
+        settings = { punchy.punch = lib.mkIf (!isLightHouse) true; };
 
         firewall = rec {
-          inbound = [
-            {
-              host = "any";
-              port = "any";
-              proto = "any";
-            }
-          ];
+          inbound = [{
+            host = "any";
+            port = "any";
+            proto = "any";
+          }];
 
           outbound = inbound;
         };
