9aa0b37a46a8541703448ce7446a18d72dcbb2e1
DestinyofYeet <ole@ole.blue>
2025-12-29 11:02:50 +0100

    chore(ankisync): re-configures anky

--- hosts/nix-server/services/ankisync.nix
+++ hosts/nix-server/services/ankisync.nix
@@ -1,31 +1,25 @@
-{
-  config,
-  lib,
-  ...
-}:
-{
+{ config, lib, secretStore, ... }:
+let secrets = secretStore.getServerSecrets "nix-server";
+in {
   age.secrets = {
-    users-ole-password = {
-      file = ./../secrets/ankisync-users-ole.age;
-    };
+    users-ole-password = { file = secrets + "/ankisync-users-ole.age"; };
   };
 
   services.anki-sync-server = {
     enable = true;
     address = "127.0.0.1";
 
-    users = [
-      {
-        username = "ole";
-        passwordFile = config.age.secrets.users-ole-password.path;
-      }
-    ];
+    users = [{
+      username = "ole";
+      passwordFile = config.age.secrets.users-ole-password.path;
+    }];
   };
 
   services.nginx.virtualHosts."ankisync.local.ole.blue" =
-    lib.custom.settings.${config.networking.hostName}.nginx-local-ssl
-    // {
+    lib.custom.settings.${config.networking.hostName}.nginx-local-ssl // {
       locations."/".proxyPass =
-        "http://${config.services.anki-sync-server.address}:${toString config.services.anki-sync-server.port}";
+        "http://${config.services.anki-sync-server.address}:${
+          toString config.services.anki-sync-server.port
+        }";
     };
 }
