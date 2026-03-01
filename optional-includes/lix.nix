764da37d6c1ec232efd812464238e6af77ba0d55
DestinyofYeet <ole@ole.blue>
2026-03-01 23:38:21 +0100

    fix(optionals):fixes lix being stupid

--- optional-includes/lix.nix
+++ optional-includes/lix.nix
@@ -1,4 +1,4 @@
 { lib, config, ... }:
-(lib.mkIf (config.capabilities.customNixInterpreter.enable) {
-  lix.enable = (builtins.trace "Enabling lix" (lib.mkForce true));
+(lib.mkIf (!config.capabilities.customNixInterpreter.enable) {
+  lix.enable = (lib.mkForce false);
 })
