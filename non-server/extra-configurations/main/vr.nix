26492ed1ad845ece6bbaaec0c344bd745820c108
DestinyofYeet <ole@ole.blue>
2026-03-21 23:32:47 +0100

    chore(vr): wivrn

--- non-server/extra-configurations/main/vr.nix
+++ non-server/extra-configurations/main/vr.nix
@@ -1,15 +1,21 @@
-{ pkgs, config, ... }: {
-  environment.systemPackages = with pkgs; [ opencomposite wayvr ];
+{ pkgs, config, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    opencomposite
+    wayvr
+  ];
 
   programs.alvr = {
     enable = true;
     openFirewall = true;
   };
 
-  programs.envision = { enable = false; };
+  programs.envision = {
+    enable = false;
+  };
 
   services.wivrn = {
-    enable = false;
+    enable = true;
     openFirewall = true;
 
     # package = pkgs.wivrn.overrideAttrs {
@@ -30,26 +36,26 @@
     autoStart = true;
 
     # Config for WiVRn (https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md)
-    config = {
-      enable = true;
-      json = {
-        # 1.0x foveation scaling
-        scale = 1.0;
-        # 100 Mb/s
-        bitrate = 120000000;
-        encoders = [{
-          # encoder = "vaapi";
-          # codec = "h265";
-          encoder = "x264";
-          codec = "h265";
-          # 1.0 x 1.0 scaling
-          width = 1.0;
-          height = 1.0;
-          offset_x = 0.0;
-          offset_y = 0.0;
-        }];
-      };
-    };
+    # config = {
+    #   enable = true;
+    #   json = {
+    #     # 1.0x foveation scaling
+    #     scale = 1.0;
+    #     # 100 Mb/s
+    #     bitrate = 120000000;
+    #     encoders = [{
+    #       # encoder = "vaapi";
+    #       # codec = "h265";
+    #       encoder = "x264";
+    #       codec = "h265";
+    #       # 1.0 x 1.0 scaling
+    #       width = 1.0;
+    #       height = 1.0;
+    #       offset_x = 0.0;
+    #       offset_y = 0.0;
+    #     }];
+    #   };
+    # };
   };
 
   boot.extraModulePackages = [
@@ -57,8 +63,7 @@
       inherit (config.boot.kernelPackages) kernel;
       patches = [
         (pkgs.fetchpatch {
-          url =
-            "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+          url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
           hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
         })
       ];
