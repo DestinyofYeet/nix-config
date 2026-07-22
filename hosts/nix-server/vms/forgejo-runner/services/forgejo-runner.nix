b6d9c211742ce54004112b0778756f171914f61e
DestinyofYeet <ole@ole.blue>
2026-07-22 22:42:47 +0200

    fix(forgejo-runner): use forgejo url

--- hosts/nix-server/vms/forgejo-runner/services/forgejo-runner.nix
+++ hosts/nix-server/vms/forgejo-runner/services/forgejo-runner.nix
@@ -2,6 +2,7 @@
   secretStore,
   config,
   pkgs,
+  flake,
   ...
 }:
 let
@@ -12,10 +13,12 @@ in
     forgejo-runner-registration-token.file = secrets.getSecret "forgejo-registration-token";
   };
 
-  services.gitea-actions-runner.instances."code-ole-blue" = {
+  services.gitea-actions-runner.instances."git-ole-blue" = {
     enable = true;
 
-    url = "https://code.ole.blue";
+    url = "https://${
+      flake.nixosConfigurations."teapot".config.services.forgejo.settings.DEFAULT.APP_NAME
+    }";
     name = "nix-server";
     labels = [ "native:host" ];
     tokenFile = config.age.secrets.forgejo-runner-registration-token.path;
