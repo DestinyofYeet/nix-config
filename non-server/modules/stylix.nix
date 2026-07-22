07273382f1de1c10c9cff5acebba783d9fbb12ef
DestinyofYeet <ole@ole.blue>
2026-07-22 19:02:46 +0200

    chore(stylix): disables overlays to fix home-manager warning

--- non-server/modules/stylix.nix
+++ non-server/modules/stylix.nix
@@ -5,47 +5,51 @@
   ...
 }:
 {
-  stylix.enable = true;
+  stylix = {
+    enable = true;
+    overlays.enable = false;
+    # me likey
+    # catppucin-frappe
+    # nebula
+    # tokyo-night-terminal-dark
+    # synth-midnight-dark
+    # tokyo-city-dark
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-terminal-dark.yaml";
 
-  # me likey
-  # catppucin-frappe
-  # nebula
-  # tokyo-night-terminal-dark
-  # synth-midnight-dark
-  # tokyo-city-dark
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-terminal-dark.yaml";
+    targets = {
+      kde.enable = false;
+      kitty = {
+        enable = true;
+        variant256Colors = true;
+      };
+
+      hyprland.enable = false;
+      hyprpaper.enable = false;
+      hyprlock.enable = false;
+
+      helix.enable = false;
+
+      waybar.enable = false;
+
+      rofi.enable = false;
+
+      swaylock.enable = false;
+
+      vim.enable = false;
+      neovim.enable = false;
+      nixvim.enable = false;
+      firefox.enable = false;
+
+      yazi.enable = false;
+      ashell.enable = false;
+      noctalia.enable = false;
+    };
+  };
+
   #
   # stylix.image = pkgs.fetchurl {
   #   url = "https://w.wallhaven.cc/full/qz/wallhaven-qzq1p5.jpg";
   #   sha256 = "sha256-iGVndavzet3G3NgpT8XGSDW6wi5eRD2SrwnJwsQqAUs=";
   # };
 
-  stylix.targets = {
-    kde.enable = false;
-    kitty = {
-      enable = true;
-      variant256Colors = true;
-    };
-
-    hyprland.enable = false;
-    hyprpaper.enable = false;
-    hyprlock.enable = false;
-
-    helix.enable = false;
-
-    waybar.enable = false;
-
-    rofi.enable = false;
-
-    swaylock.enable = false;
-
-    vim.enable = false;
-    neovim.enable = false;
-    nixvim.enable = false;
-    firefox.enable = false;
-
-    yazi.enable = false;
-    ashell.enable = false;
-    noctalia.enable = false;
-  };
 }
