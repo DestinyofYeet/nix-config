25477faf2f82e1b4e4255c67f46b329b080b4859
DestinyofYeet <ole@ole.blue>
2026-08-06 21:43:07 +0200

    chore(lix): remove

--- flake.nix
+++ flake.nix
@@ -170,16 +170,16 @@
 
     tiddl-nix.url = "github:DestinyofYeet/tiddl.nix";
 
-    lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
+    # lix = {
+    #   url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+    #   flake = false;
+    # };
 
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.lix.follows = "lix";
-    };
+    # lix-module = {
+    #   url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   inputs.lix.follows = "lix";
+    # };
 
     authentik-nix.url = "github:nix-community/authentik-nix";
 
