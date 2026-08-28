55c3b0b04a36ebeb9399645d33674907a4212083
DestinyofYeet <ole@ole.blue>
2026-08-28 16:25:38 +0200

    chore(ha-vm): removed bonk from nodes

--- hosts/parts/ha-vm/services/etcd.nix
+++ hosts/parts/ha-vm/services/etcd.nix
@@ -16,7 +16,7 @@ in
     initialCluster = [
       "nix-server-ha-vm=http://${(machines."nix-server-ha-vm").ip}:2380"
       "teapot-ha-vm=http://${(machines."teapot-ha-vm").ip}:2380"
-      "bonk-ha-vm=http://${(machines."bonk-ha-vm").ip}:2380"
+      # "bonk-ha-vm=http://${(machines."bonk-ha-vm").ip}:2380"
       "hope=http://${(machines."hope").ip}:2380"
     ];
   };
